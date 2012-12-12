7dd1041afa32ca28fca513d70ea1f651d93730f8
Bobby Benton <bobby@planettelex.net>
2012-12-12 13:40:08 -0700

    added styling fixes for various issues, cleaned up JS in ResponsiveChanges.js, removed bug in Document.cshtml

--- TheThemeMachineResponsive/Scripts/ResponsiveChanges.js
+++ TheThemeMachineResponsive/Scripts/ResponsiveChanges.js
@@ -7,8 +7,14 @@ $(function () {
 
     // Function to collapse main navigation and create dropdown with icon
     function responsiveNavigation() {
+
+        // Hide main navigation if visible
         $('#layout-navigation').hide();
+
+        // Create collapsed navigation button with three lines and "Menu" text
         $('<a href="#" id="nav-collapsed"><span class="responsive-nav-bars"></span><span class="responsive-nav-bars"></span><span class="responsive-nav-bars"></span><span class="responsive-menu-text">Menu</span></a>').appendTo('.zone-header');
+
+        // Toggle navigation visibility when collapsed navigation button is clicked
         $('#nav-collapsed').click(function () {
             $('#layout-navigation').toggle();
             if ($('#layout-navigation').is(':visible')) {
@@ -17,12 +23,13 @@ $(function () {
                 $('#nav-collapsed').removeClass('is-active');
             }
         });
+
     };
 
     /* Append/remove responsive navigation based on breakpoints when document loads */
     if ($(window).width() <= 768) {
         responsiveNavigation();
-    } else if ($(window).width() > 768) {
+    } else {
         $('#nav-collapsed').remove();
         $('#layout-navigation').show();
     };
@@ -38,15 +45,14 @@ $(function () {
     });
     /* End responsive navigation */
 
-    /* Duplicate main navigation in Footer (this comes in handy when scrolling to the bottom on mobile device)
-    ** Remove if not desired.
-    */
-    $('#layout-navigation').clone().insertBefore('#footer-sig').addClass('footer-menu');
-    /* End duplicated main navigation in footer */
+    /**************************
+    Optional changes common to mobile sites.
+    ***************************/
 
-    /* Place first aside below layout-content on mobile browsers (aside's are generally less important than main content so this might make sense)
-    ** Remove in not desired.
-    */
+    /* Duplicate main navigation in Footer (this comes in handy when scrolling to the bottom on mobile device) */
+    $('#layout-navigation').clone().insertBefore('#footer-sig').addClass('footer-menu');
+
+    /* Place first aside below layout-content on mobile browsers (aside's are generally less important than main content so this might make sense) */
     if ($(window).width() <= 768 && $('#aside-first').length == 1) {
         $('#aside-first').insertAfter('#layout-content');
     }
